edaaf26c401aad950e3599b3f72aa75cadb99fb4
miiiiiko <97025329+miiiiiko@users.noreply.github.com>
2022-12-21 19:57:28 +0800

    Add files via upload

---
+++ utils/pred_helper.py
@@ -0,0 +1,77 @@
+import sys
+sys.path.append('./')
+import torch
+from utils import ljqpy
+from utils.sortlabel import llist
+
+
+all_data = ljqpy.LoadJsons('./dataset/train.json')
+label_matrix = torch.zeros(1400,1400)
+
+# 计算共现矩阵，将总是一起出现的标签对绑定在一起
+for d in all_data:
+    label_list = d['label']
+    for i in range(len(label_list)):
+        for j in range(len(label_list)):
+            l1 = llist.get_id(label_list[i])
+            l2 = llist.get_id(label_list[j])
+            label_matrix[l1][l2] += 1
+
+label_matrix = torch.stack([ele/ele.sum() for ele in label_matrix])
+label_pair = (label_matrix == 1).nonzero()
+# print(len(label_pair))
+# print(label_pair)
+def transfer(scores):
+    '''
+    scores是batch_size*n_labels的tensor，上已经计算总是一起出现的标签对label_pair，
+    对于每一对标签，在推理时该标签的分数为该标签对分数较大的那个
+    '''
+    for i in range(len(label_pair)):
+        i,j = label_pair[i][0],label_pair[i][1]
+        res = torch.max(scores[...,i],scores[...,j])
+        scores[...,i],scores[...,j] = res,res
+    return scores
+# scores = torch.tensor([[0]*1400,[1]*1400])
+
+# transfer(scores)
+def transfer_z(z):
+    '''
+    如果一个标签为1，标签对都应被判断为1
+    '''
+    for i,j in label_pair:
+        if z[i] == 1 or z[j] == 1:
+            z[i],z[j] = 1,1
+    return z
+
+# 人工添加了一些规则，对于模型仍然预测不好的标签，找出其关键词，进行硬匹配
+lab2word = {'label_895459':'入境','label_582805':'情侣头像','label_493566':'萧邦',
+            'label_766890':'单身久了', 'label_819030':'张俪','label_1219604':'堂食','label_1340977':'民族匠心','label_361083':'反诈老陈',
+            'label_658167':'迷你世界','label_137439':'恋恋北极星','label_1390048':'航空公司','label_207137':'李紫婷','label_161121':'金译团',
+            'label_1003895':'光子','label_161726': '爱情电影频道','label_102402': '六一儿童节',"label_1064693":"韩国炸鸡",'label_312604':'华为深圳','label_596430':'榴莲',
+            'label_1516519':'姜栋元','label_247451':'天猫618','label_1313425':'天猫618','label_206611':'小麦','label_1429838':'夏娃','label_714824':'金湾'}
+
+
+def check_text(text,lab2word=lab2word):
+    '''
+    检查文本，若文本含有关键词，即判断文本有该关键词对应的标签
+    '''
+    indices = []
+    for k,v in lab2word.items():
+        if v in text:
+            indices.append(llist.get_id(k))
+    return indices
+
+
+class Val0_Classify():
+    def __init__(self,llist):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.tl = llist
+        self.fun = self.argmax_func
+
+       
+    def argmax_func(self,score_i):
+        pred_vec = torch.zeros_like(score_i)
+        threshold = score_i.max()*0.4
+        for idx in (score_i > threshold).float().nonzero().squeeze(1):
+            pred_vec[idx] = 1
+        return pred_vec
---
+++ utils/sortlabel.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append('./')
+import os
+import numpy
+from collections import defaultdict
+from utils.ljqpy import LoadJsons,TokenList,FreqDict2List,SaveCSV,LoadCSV
+# import torch
+
+class TokenList:
+    def __init__(self, file, low_freq=1, source=None, func=None, save_low_freq=1, special_marks=[]):
+        if not os.path.exists(file):
+            tdict = defaultdict(int)
+            for i, xx in enumerate(special_marks): tdict[xx] = 100000000 - i
+            for xx in source:
+                for token in func(xx): tdict[token] += 1
+            tokens = FreqDict2List(tdict)  # already decline sorted
+            tokens = [x for x in tokens if x[1] >= save_low_freq]
+            SaveCSV(tokens, file)
+        self.id2t = [ '<UNK>'] + [x for x,y in LoadCSV(file) if float(y) >= low_freq]
+        self.t2id = {v:k for k,v in enumerate(self.id2t)}
+        self.id2freq = [int(y) for x,y in LoadCSV(file) if float(y) >= low_freq]
+        self.freq2id = {v:k+1 for k,v in enumerate(self.id2freq)}
+    def get_id(self, token): return self.t2id.get(token, -1)
+    def get_token(self, ii): return self.id2t[ii]
+    def get_num(self): return len(self.id2t)
+
+def label2vec(targrtlabels:list,llist:TokenList):
+    lab_vec = numpy.zeros(llist.get_num())
+    for label in targrtlabels:
+        loc = llist.get_id(label)
+        lab_vec[loc] = 1.0
+    return lab_vec
+
+def label2id(targrtlabels:list,llist:TokenList):
+    locs = []
+    for label in targrtlabels:
+        locs.append(llist.get_id(label))
+    return locs
+def loc(num:int, llist:TokenList):
+    '''
+    返回llist中第一个频数为num的id，llist[:x]得到的是频数大于num的列表，[x:]则为小于等于
+    '''
+    for inx,i in enumerate(llist.id2freq): 
+        if i < num+1: return inx+1
+        elif inx == len(llist.id2freq)-1: return inx+1
+def sep_point(freq_list:list):
+    return [loc(item) for item in freq_list]
+
+source = LoadJsons('./dataset/train.json')
+llist = TokenList('sortlabel.txt', source=source, func=lambda x:x['label'], low_freq=1, save_low_freq=1)
