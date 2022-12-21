bd9e61f3b564b45285fa0ca3866b4b85025fcd2f
HenrynsXu <116343602+HenrynsXu@users.noreply.github.com>
2022-12-21 21:11:50 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ class MLP(nn.Module):
     def __init__(self, n_in, n_out, activation=False): 
         super().__init__()
         self.linear = nn.Linear(n_in, n_out)
-        if activation:
+        if activation:  # 是否加入sigmoid层
             self.activation = nn.Sigmoid()
         else:
             self.activation = None
@@ -19,7 +19,7 @@ class MLP(nn.Module):
         return x
 
 
-class Model(nn.Module):
+class Model(nn.Module):  # 模型基础结构，Bert+线性全连接层
     def __init__(self, model_name='hfl/chinese-roberta-wwm-ext',n=1400,encoder_type = 'cls',activation=False,mfile0 = None):
         super(Model, self).__init__()
         self.encoder_type = encoder_type
